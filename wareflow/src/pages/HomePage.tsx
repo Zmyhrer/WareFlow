de1e1e5c0d6155d3421211bd0bbdc7ff21c0f88c
Zmyhrer <zachs.myhrer@gmail.com>
2024-09-29 00:03:29 -0500

    Modified Header and created Sidebar with menu

--- wareflow/src/pages/HomePage.tsx
+++ wareflow/src/pages/HomePage.tsx
@@ -3,11 +3,17 @@
 import React from "react";
 import "./HomePage.css";
 import Header from "@/components/Header1";
+import Sidebar from "@/components/Sidebar";
 
 const HomePage: React.FC = () => {
   return (
     <div className="homepage-container">
       <Header />
+
+      <div className="middle-content">
+        <Sidebar />
+        <div className="main-content"></div>
+      </div>
     </div>
   );
 };
